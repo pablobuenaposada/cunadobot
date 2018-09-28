d692b63770a26c2b212c7aee94c5e6c17ff1adde
Toni Colom <toni@semicolom.com>
2018-09-28 17:35:39 +0200

    Adds settings example file, closes #8

---
+++ settings.example.py
@@ -0,0 +1,4 @@
+SLACK_URL = "https://hooks.slack.com/services/#your_slack_url"
+SLACK_CHANNEL = "#your_channel"
+CREDENTIALS_FILE = "/home/pi/Desktop/cunao_credentials.json"
+SHEET_NAME = "Cunadobot"
