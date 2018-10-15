cb7f6b0d4dc17f93269c9d1ad9696da52582fe14
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2018-10-15 14:45:58 +0200

    Closes #24 delete settings.py
    Since we have settings.example.py we don't need this anymore, make install will already create a new settings.py

--- settings.py
+++
@@ -1,4 +0,0 @@
-SLACK_URL = "https://hooks.slack.com/services/#your_slack_url"
-SLACK_CHANNEL = "#your_channel"
-CREDENTIALS_FILE = "/home/pi/Desktop/cunao_credentials.json"
-SHEET_NAME = "Cunadobot"
