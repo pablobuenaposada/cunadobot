07f23be3323f731c4f308d5e2a1f3ed5232c23ce
Toni Colom <toni@semicolom.com>
2018-10-17 16:12:51 +0200

    Creates a separate function to write a quote to slack

--- tests/test_cunao.py
+++ tests/test_cunao.py
@@ -1,6 +1,15 @@
 import cunao
+from mock import patch
 
 
 def test_get_phrases():
     phrases = cunao.get_phrases()
+
     assert phrases == [{'Quote': 'Con una cerveza no das positivo'}]
+
+
+def test_write_quote_to_slack():
+    with patch('cunao.requests.post') as mock_get:
+        cunao.write_quote_to_slack('test quote')
+
+        assert mock_get.called is True
