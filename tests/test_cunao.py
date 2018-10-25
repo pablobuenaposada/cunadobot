516fb0764ac2a55507c18de4935d00879979380d
Pablo Buenaposada <pablo.buenaposada@elementsinteractive.es>
2018-10-25 11:16:55 +0200

    Multiple slack urls
    Now the setting SLACK_URL is a list allowing to send the quote to more than one slack webhook url

--- tests/test_cunao.py
+++ tests/test_cunao.py
@@ -1,13 +1,31 @@
+import settings
 from src.cunao import get_quotes, write_quote_to_slack
-from mock import patch
+from mock import call, patch
+
+TEST_QUOTE = 'Con una cerveza no das positivo'
 
 
 def test_get_quotes():
     quotes = get_quotes()
-    assert quotes == [{'Quote': 'Con una cerveza no das positivo'}]
+    assert quotes == [{'Quote': TEST_QUOTE}]
 
 
 def test_write_quote_to_slack():
     with patch('src.cunao.requests.post') as mock_get:
-        write_quote_to_slack('test quote')
+        write_quote_to_slack(TEST_QUOTE)
         assert mock_get.called
+
+
+def test_write_quote_to_slack_with_more_tha_one_url():
+    """
+    Check if the quote is sent to all the slack urls provided
+    """
+    settings.SLACK_URLS = ['http://www.url1.com', 'http://www.url2.com']
+    expected_calls = [call('http://www.url1.com', json={'text': TEST_QUOTE.upper()}),
+                      call('http://www.url2.com', json={'text': TEST_QUOTE.upper()})]
+
+    with patch('src.cunao.requests.post') as mock_get:
+        write_quote_to_slack(TEST_QUOTE)
+
+        assert mock_get.call_count is len(expected_calls)
+        mock_get.assert_has_calls(expected_calls, any_order=True)
