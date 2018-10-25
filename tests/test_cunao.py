1b6ba9b4d3c0f4734bf5ec2eff39bd5222052d5b
Toni Colom <toni@semicolom.com>
2018-10-25 10:32:00 +0200

    Closes #28 replacing phrase word by quote

--- tests/test_cunao.py
+++ tests/test_cunao.py
@@ -1,10 +1,10 @@
-from src.cunao import get_phrases, write_quote_to_slack
+from src.cunao import get_quotes, write_quote_to_slack
 from mock import patch
 
 
-def test_get_phrases():
-    phrases = get_phrases()
-    assert phrases == [{'Quote': 'Con una cerveza no das positivo'}]
+def test_get_quotes():
+    quotes = get_quotes()
+    assert quotes == [{'Quote': 'Con una cerveza no das positivo'}]
 
 
 def test_write_quote_to_slack():
